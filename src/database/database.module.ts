20eee93e5b2054c285f9d42ee23af5f756b69ad3
Ahtasham Faheem <ahtashamfaheem8@gmail.com>
2025-10-02 19:43:04 +0500

    code

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,16 +7,17 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        const uri = 'mongodb+srv://launchixai_db_user:TdKQsrePYY2a3mY1@launchix0.0pblfpo.mongodb.net/launchix?retryWrites=true&w=majority&appName=launchix0';
-        return {
-          uri,
-        };
-      },
-      inject: [ConfigService],
-    }), 
+    // MongooseModule.forRootAsync({
+    //   imports: [ConfigModule],
+    //   useFactory: async (configService: ConfigService) => {
+    //     const uri = 'mongodb+srv://launchixai_db_user:TdKQsrePYY2a3mY1@launchix0.0pblfpo.mongodb.net/launchix?retryWrites=true&w=majority&appName=launchix0';
+    //     return {
+    //       uri,
+    //     };
+    //   },
+    //   inject: [ConfigService],
+    // }), 
+    MongooseModule.forRoot('mongodb+srv://launchixai_db_user:TdKQsrePYY2a3mY1@launchix0.0pblfpo.mongodb.net/launchix?retryWrites=true&w=majority&appName=launchix0')
   ],
 })
 export class DatabaseModule {}
