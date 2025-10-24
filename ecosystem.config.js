70e55a3611a1e19d07b8046f1e413615110408c9
Ahtasham Faheem <92648099+Ahtasham-Faheem@users.noreply.github.com>
2025-10-24 15:54:47 +0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,6 @@ module.exports = {
         NODE_ENV: "production",
         MONGO_URL: "mongodb+srv://launchixai_db_user:TdKQsrePYY2a3mY1@launchix0.0pblfpo.mongodb.net/",
         PORT: 5000
-        
       }
     }
   ]
