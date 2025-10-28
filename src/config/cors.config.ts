d46c7cc73b7b4bc849b95ba0690b960342d542ce
Aqib Ijaz <aqibijaz3@gmail.com>
2025-10-28 15:37:25 +0500

    del old code

--- src/config/cors.config.ts
+++
@@ -1,9 +0,0 @@
-export const corsOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : [
-       'http://localhost:3000',     // Local development
-      'http://localhost:5173',     // Vite default port
-      'http://3.92.141.250:3000',
-      'http://3.92.141.250:5173',
-      'https://platform.launchix.ai'
-    ];
--- src/printify/printify.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrintifyController } from './printify.controller';
-import { PrintifyService } from './printify.service';
-
-@Module({
-  controllers: [PrintifyController],
-  providers: [PrintifyService],
-})
-export class PrintifyModule {}
